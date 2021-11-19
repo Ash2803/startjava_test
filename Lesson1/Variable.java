b482df18ae9e1fea79a3e04e7bd5b7de1a768f9d
Ash <ashot.93@mail.ru>
2021-11-20 01:13:56 +0300

    FirstHW

---
+++ Lesson1/Variable.java
@@ -0,0 +1,25 @@
+public class Variable {
+	
+	public static void main(String[] args) 	{
+
+		byte cpu = 8;
+		short ram = 8;
+		int vram = 6;
+		long battery = 4200;
+		float ghz = 8.5F;
+		double windows = 11.11;
+		char notebook = 'g';
+		boolean b = true;
+
+		System.out.println(cpu);
+		System.out.println(ram);
+		System.out.println(vram);
+		System.out.println(battery);
+		System.out.println(ghz);
+		System.out.println(windows);
+		System.out.println(notebook);
+		System.out.println(b);
+		System.out.println("DONE)");
+
+	}
+}
