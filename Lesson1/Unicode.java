18331347645afbfd44a9eab8b9d07a0c8e4ec079
Ash <ashot.93@mail.ru>
2021-11-24 22:35:10 +0300

    Task #5

---
+++ Lesson1/Unicode.java
@@ -0,0 +1,8 @@
+public class Unicode {
+    public static void main(String[] args) {
+        for(int symbol = 33; symbol <=126; symbol++) {
+            char ch = (char)symbol;
+            System.out.println(ch);
+        }
+    }
+}
