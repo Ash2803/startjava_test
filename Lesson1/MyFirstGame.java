6ef8582520325833cc7867f5af167c0e67b3b55b
Ash <ashot.93@mail.ru>
2021-11-24 21:32:30 +0300

    Task 5

---
+++ Lesson1/MyFirstGame.java
@@ -0,0 +1,25 @@
+public class MyFirstGame
+{
+    public static void main(String[] args) {
+        
+        int compNum = (int) ( Math.random() * 100);
+        int playerNum = (int) ( Math.random() * 100);
+        
+        while(compNum != playerNum) {
+          
+          
+         if(compNum > playerNum) {
+             playerNum++;
+            System.out.println("Данное число меньше того, что загадал компьютер");
+            
+        } else if(compNum < playerNum) {
+            playerNum--;
+            System.out.println("Данное число больше того, что загадал компьютер");
+        } 
+        }
+
+        if(compNum == playerNum) {
+            System.out.println("Поздравляю, число угадано!");
+        }
+    }
+}
