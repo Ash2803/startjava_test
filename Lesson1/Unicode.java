671b10c3cd9329b211fb792d7c4173968f3f9ab7
Ash <ashot.93@mail.ru>
2021-11-27 00:00:09 +0300

    Fixed

--- Lesson1/Unicode.java
+++ Lesson1/Unicode.java
@@ -1,8 +1,7 @@
 public class Unicode {
     public static void main(String[] args) {
-        for(int symbol = 33; symbol <=126; symbol++) {
-            char ch = (char)symbol;
-            System.out.println(ch);
+        for(int symbol = 33; symbol <= 126; symbol++) {
+            System.out.println((char) symbol);
         }
     }
 }
