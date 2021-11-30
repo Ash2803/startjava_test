7d9deea3cd6c8d505093ff135c71c4ba82b70264
Ash2803 <92327661+Ash2803@users.noreply.github.com>
2021-11-30 12:20:26 +0300

    Update MyFirstGame.java

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,21 +1,17 @@
-public class MyFirstGame
-{
+public class MyFirstGame {
     public static void main(String[] args) {
         
-        int compNum = (int) ( Math.random() * 100);
-        int playerNum = (int) ( Math.random() * 100);
-        
+        int compNum = (int) (Math.random() * 100);
+        int playerNum = (int) (Math.random() * 100);
         while(compNum != playerNum) {
           
-          
-         if(compNum > playerNum) {
-             playerNum++;
+        if(compNum > playerNum) {
+            playerNum++;
             System.out.println("Данное число меньше того, что загадал компьютер");
-            
         } else if(compNum < playerNum) {
             playerNum--;
             System.out.println("Данное число больше того, что загадал компьютер");
-        } 
+            } 
         }
 
         if(compNum == playerNum) {
