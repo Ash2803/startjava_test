585e8e3d2e55a053398fbf668b0a57f59d95434f
Ash2803 <92327661+Ash2803@users.noreply.github.com>
2021-12-06 11:42:43 +0300

    Update CalculatorTest.java

--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -6,8 +6,8 @@ public class CalculatorTest {
         Calculator calcOne = new Calculator();
         Scanner scanner = new Scanner(System.in);
 
-        boolean right = true;
-        while(right) {
+            boolean done = true;
+            while(done) {
             System.out.println("Enter the first number");
             calcOne.setA(scanner.nextInt());
             System.out.println("Enter the math sign");
@@ -17,17 +17,23 @@ public class CalculatorTest {
             System.out.println(calcOne.getResult());
             System.out.println("Do you want to continue? [Yes/No]:");
             String result = (scanner.next());
-
+ 
             if(result.equalsIgnoreCase("Yes")) {    
                 System.out.println("Great! Let's get started.");
-                right = true;
+                done = true;
             } else if(result.equalsIgnoreCase("No")) {
                 System.out.println("Goodbye!");
-                right = false;
-            } else if(!result.equalsIgnoreCase("No") && !result.equalsIgnoreCase("Yes")) {
+                done = false;
+            } while(!result.equalsIgnoreCase("No") && !result.equalsIgnoreCase("Yes")) {
                 System.out.println("Do you want to continue? [Yes/No]:");
                 result = (scanner.next());
-            } 
-        }
-    }
+            } if(result.equalsIgnoreCase("Yes")) {    
+                System.out.println("Great! Let's get started.");
+                done = true;
+        	} else if(result.equalsIgnoreCase("No")) {
+                System.out.println("Goodbye!");
+                done = false;
+    		}
+		}
+  	}
 }
