8c6b087e6be6669f94fbde4c3d9362d238a29c73
Ash <ashot.93@mail.ru>
2021-12-12 21:43:35 +0300

    My game v3 fixed

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -4,7 +4,7 @@ public class GuessNumber {
 
     private Player pl1;
     private Player pl2;
-    int gameNum = (int) (Math.random() * 100);
+    private int targetNumber = (int) (Math.random() * 100);
 
     public GuessNumber(Player pl1,  Player pl2) {
         this.pl1 = pl1;
@@ -13,28 +13,31 @@ public class GuessNumber {
 
     public void start() {
         Scanner sc = new Scanner(System.in);
-            while (true) {
-                System.out.println("Player 1 enter the number: ");
-                pl1.setNum(sc.nextInt());
-                if(gameNum > pl1.getNum()) {
-                    System.out.println("This number is lower then computer's");
-                } else if(gameNum < pl1.getNum()) {
-                    System.out.println("This number is higher then computer's");
-                } else if(gameNum == pl1.getNum()) {
-                    System.out.println(pl1.getName() + " win");
-                    break;
-                }
-                System.out.println("Player 2 enter the number: ");
-                pl2.setNum(sc.nextInt());
-                if((gameNum > pl2.getNum())) {
-                    System.out.println("This number is lower then computer's");
-                } else if(gameNum < pl2.getNum()) {
-                    System.out.println("This number is higher then computer's");
-                } else if(gameNum == pl2.getNum()) {
-                    System.out.println(pl2.getName() + " win");
-                    break;
-                }
+        while (true) {
+            System.out.println("Player 1 enter the number: ");
+            pl1.setNum(sc.nextInt());
+
+            if(targetNumber > pl1.getNum()) {
+                System.out.println("This number is lower then computer's");
+            } else if(targetNumber < pl1.getNum()) {
+                System.out.println("This number is higher then computer's");
+            } else if(targetNumber == pl1.getNum()) {
+                System.out.println(pl1.getName() + " win");
+                break;
+            }
+
+            System.out.println("Player 2 enter the number: ");
+            pl2.setNum(sc.nextInt());
+
+            if((targetNumber > pl2.getNum())) {
+                System.out.println("This number is lower then computer's");
+            } else if(targetNumber < pl2.getNum()) {
+                System.out.println("This number is higher then computer's");
+            } else if(targetNumber == pl2.getNum()) {
+                System.out.println(pl2.getName() + " win");
+                break;
             }
         }
     }
+}
 
